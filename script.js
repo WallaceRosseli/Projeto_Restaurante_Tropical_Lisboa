0e78f22aedfee922d5a5810f4070701e7bd431f2
WallaceRosseli <wc_rosseli_@hotmail.com>
2025-06-09 11:03:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mascara_tel (objeto){
+    if(objeto.value.length == 0){
+        objeto.value = objeto.value + '(';
+    }
+    if(objeto.value.length == 3){
+        objeto.value = objeto.value + ')';
+    }
+    if(objeto.value.length == 9){
+        objeto.value = objeto.value + '-';
+    }
+}
